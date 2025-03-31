6dd198960892c7a4f878c00a74e3a3f51d18a227
Rati Montreewat <ratixoxo@gmail.com>
2025-03-31 20:50:04 +0700

    Fix (./sr): Fixing ERC4626 && its path

--- src/MyERC4626.sol
+++ src/MyERC4626.sol
@@ -1,15 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin-5_2_0/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import {IERC20} from "@openzeppelin-5_2_0/contracts/token/ERC20/IERC20.sol";
 
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20} from "@openzeppelin-5_2_0/contracts/token/ERC20/ERC20.sol";
+import {ERC4626} from "@openzeppelin-5_2_0/contracts/token/ERC20/extensions/ERC4626.sol";
+
+import {Ownable} from "@openzeppelin-5_2_0/contracts/access/Ownable.sol";
 
 
 contract MyERC4626 is ERC4626, Ownable {
+
+    using SafeERC20 for IERC20;
 
     constructor(
         IERC20 asset,
